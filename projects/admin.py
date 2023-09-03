367af665e68f212a27b6abae82ddc2d5fbec98ad
Md Akther Hosen <dev.aktherhosen@gmail.com>
2023-09-03 20:40:44 +0600

    done

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import PortfolioProject,Category,ProjectReview
+from . models import PortfolioProject,Category, ProjectReview
 # Register your models here.
 
 admin.site.register(Category)
