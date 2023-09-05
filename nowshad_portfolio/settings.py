871a804bec16fc36759962abc9c843024f899ebf
Md Akther Hosen <dev.aktherhosen@gmail.com>
2023-09-05 12:06:58 +0600

    updated

--- nowshad_portfolio/settings.py
+++ nowshad_portfolio/settings.py
@@ -120,8 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+    
+if not DEBUG:
+    STATIC_ROOT = ''
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+    
 STATIC_ROOT = BASE_DIR/ 'static'
-print(STATIC_ROOT)
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
